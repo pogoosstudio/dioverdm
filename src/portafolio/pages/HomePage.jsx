7d306525b2c35e775808cde8a6148b9d3e831c47
pogoosstudio <dioverrdm@gmail.com>
2024-08-03 16:12:19 -0400

    Update HomePage.jsx

--- src/portafolio/pages/HomePage.jsx
+++ src/portafolio/pages/HomePage.jsx
@@ -10,7 +10,7 @@ export const HomePage = () => {
             initial={{ scale: 0.5 }}
             animate={{ scale: 1 }}    
         >
-            <h1 className={`${clases.page__name}`}>Nicolás Hernández</h1>
+            <h1 className={`${clases.page__name}`}>Diover Rivero</h1>
             <h2 className={`${clases.page__job}`}>Desarrollador Web</h2>
         </motion.section>
     );
