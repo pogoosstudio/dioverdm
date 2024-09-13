775d3ef30175b0af2953887a938c3f692b5c3371
pogoosstudio <dioverrdm@gmail.com>
2024-09-13 09:01:37 -0400

    Update SobreMiPage.jsx

--- src/portafolio/pages/SobreMiPage.jsx
+++ src/portafolio/pages/SobreMiPage.jsx
@@ -22,8 +22,8 @@ export const SobreMiPage = () => {
             <section className={`${clases.about__personalInfo}`}>
                 <article className={`${clases.personalInfo__bio}`}>
                     <p className={`${clases.personalInfo__description}`}>
-                        Hola me llamo Nicolas y me gusta el desarrollo Web, los videojuegos, el deporte
-                        y los animales. Mis hobbies actuales son el deporte y el dibujo. Mis metas
+                        Hola me llamo Diover y me gusta el desarrollo Web, los videojuegos, el deporte
+                        y los animales. Mis hobbies actuales son el deporte y la música. Mis metas
                         son dominar JavaScript con CSS y seguir aprendiendo mas sobre 
                         las tecnologias Front-End. 
                     </p>
@@ -32,23 +32,23 @@ export const SobreMiPage = () => {
                     <ul className={`${clases.personalInfo__list}`}>
                         <li className={`${clases.personalInfo__option}`}>
                             <span className={`${clases.personalInfo__title}`}>Edad</span>
-                            <span className={`${clases.personalInfo__value}`}>24</span>
+                            <span className={`${clases.personalInfo__value}`}>19</span>
                         </li>
                         <li className={`${clases.personalInfo__option}`}>
                             <span className={`${clases.personalInfo__title}`}>Pais</span>
-                            <span className={`${clases.personalInfo__value}`}>Chile</span>
+                            <span className={`${clases.personalInfo__value}`}>Venezuela</span>
                         </li>
                         <li className={`${clases.personalInfo__option}`}>
                             <span className={`${clases.personalInfo__title}`}>Dirección</span>
-                            <span className={`${clases.personalInfo__value}`}>Santiago, Curacaví</span>
+                            <span className={`${clases.personalInfo__value}`}>Anzoátegui, Barcelona</span>
                         </li>
                         <li className={`${clases.personalInfo__option}`}>
                             <span className={`${clases.personalInfo__title}`}>Correo</span>
-                            <span className={`${clases.personalInfo__value}`}>nico.hernandezekko@gmail.com</span>
+                            <span className={`${clases.personalInfo__value}`}>hola@dioverdm.com</span>
                         </li>
                         <li className={`${clases.personalInfo__option}`}>
                             <span className={`${clases.personalInfo__title}`}>Telefono</span>
-                            <span className={`${clases.personalInfo__value}`}>+569 98180435</span>
+                            <span className={`${clases.personalInfo__value}`}>+58 424 843 3917</span>
                         </li>
                     </ul>
                 </div>
@@ -68,9 +68,9 @@ export const SobreMiPage = () => {
                         <div className={`${clases.service__content}`}>
                             <h4 className={`${clases.service__title}`}>Programación</h4>
                             <p className={`${clases.service__description}`}>
-                                Mi aprendizaje en programación comenzo en DuocUC donde aprendí 
-                                lo mas basico de Java y también a modelar y hacer consultas a la BDD. 
-                                Luego aprendí programación de BDD utlizanto PL/SQL, que fue lo 
+                                Mi aprendizaje en programación comenzo a mis 13 años en Udemy donde aprendí 
+                                lo mas basico de Javascript y también a modelar y hacer consultas a una la DB. 
+                                Luego aprendí programación de con TypeScript, que fue lo 
                                 que me inspiro a seguir en este mundo del desarrollo ya que fue un ramo 
                                 en el que lleve al maximo mis habilidaes de auto aprendizaje.
                             </p>
