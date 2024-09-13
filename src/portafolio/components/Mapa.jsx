8a80072308582ae3b8567ed5114f6c91e04f926c
pogoosstudio <dioverrdm@gmail.com>
2024-09-13 09:21:16 -0400

    Update Mapa.jsx

--- src/portafolio/components/Mapa.jsx
+++ src/portafolio/components/Mapa.jsx
@@ -13,7 +13,7 @@ export const Mapa = () => {
                 marginWidth={0} 
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade" 
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1665.022458931834!2d-71.13906005838673!3d-33.42207327621555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1721333448282!5m2!1ses!2scl"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d125691.18323305208!2d-64.6636344!3d10.1114046!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2d723eff71306f%3A0x22feb43cef889e75!2sBarcelona%2C%20Anzo%C3%A1tegui!5e0!3m2!1ses!2sve!4v1726233569641!5m2!1ses!2sve"
             >
             </iframe>
         </div>
